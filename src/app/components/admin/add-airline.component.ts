191a0017613f8dc6db7029c5996a3c7ff04d168d
kanchanraiii <kanchanrai2307@gmail.com>
2025-12-22 17:07:08 +0530

    added: add airline comp, ui

--- src/app/components/admin/add-airline.component.ts
+++ src/app/components/admin/add-airline.component.ts
@@ -1,16 +1,26 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import { AuthService } from '../../services/auth.service';
+import { RouterModule, Router } from '@angular/router';
 
 @Component({
   standalone: true,
-  imports: [CommonModule, FormsModule],
+  imports: [CommonModule, FormsModule, RouterModule],
   templateUrl:'./add-airline.html' 
 })
 export class AddAirlineComponent {
   name = '';
+  constructor(
+     private auth: AuthService,
+     private router: Router
+  ){}
 
   submit() {
     console.log('Airline added:', this.name);
+  }
+   logout() {
+    this.auth.logout();
+    this.router.navigate(['/login']);
   }
 }
